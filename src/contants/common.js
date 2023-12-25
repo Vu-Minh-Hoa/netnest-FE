bc86a1ee1621ae0a0750a6e19ea083011240d499
Hòa Vũ <hoaminhvuu@gmail.com>
2023-12-25 17:59:21 +0700

    Upload API list

--- src/contants/common.js
+++ src/contants/common.js
@@ -5,4 +5,20 @@ export const NAV_TAB = {
 };
 
 export const BASE_API = '';
-export const API_LIST = {};
+export const API_LIST = {
+  post_user: '/users',
+  post_imgage_video: '/',
+  get_search_detail_user: '/searchDetailUser',
+  get_search_user: '/searchUser',
+  post_add_following: '/addFollowing',
+  get_following: '/following',
+  get_followers: '/followers',
+  put_update_user: '/updateUser',
+  put_update_avatar: '/updateAvatar',
+  post_create_post: '/createPost',
+  post_update_image_post: '/posts/updateImage',
+  login: '/login',
+  logout: '/logout',
+  register: '/register',
+  home: '/home',
+};
